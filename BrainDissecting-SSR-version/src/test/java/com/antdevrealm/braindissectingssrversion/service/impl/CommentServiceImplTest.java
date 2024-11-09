805897270f27e452728aeb13275e9330f5856724
AntoanYosifov <tapaktapxaomi@gmail.com>
2024-11-09 06:48:53 +0100

    CommentServiceImplTest -> delete() -> Should Return False When User Does Not Exist

--- BrainDissecting-SSR-version/src/test/java/com/antdevrealm/braindissectingssrversion/service/impl/CommentServiceImplTest.java
+++ BrainDissecting-SSR-version/src/test/java/com/antdevrealm/braindissectingssrversion/service/impl/CommentServiceImplTest.java
@@ -148,4 +148,22 @@ public class CommentServiceImplTest {
         Assertions.assertFalse(result);
     }
 
+    @Test
+    void delete_ShouldReturnFalse_WhenUserDoesNotExist() {
+        long articleId = 1L;
+        long commentId = 1L;
+        long userId = 1L;
+
+        ArticleEntity articleEntity = new ArticleEntity();
+        articleEntity.setId(articleId);
+        articleEntity.setTitle("testArticle");
+
+        when(mockArticleRepository.findById(articleId)).thenReturn(Optional.of(articleEntity));
+        when(mockUserRepository.findById(userId)).thenReturn(Optional.empty());
+
+        boolean result = toTest.delete(articleId, commentId, userId);
+
+        Assertions.assertFalse(result);
+    }
+
 }
