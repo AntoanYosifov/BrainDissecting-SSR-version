d2b9fe09bc48780b947d5ef9713048910611b03e
AntoanYosifov <tapaktapxaomi@gmail.com>
2024-11-09 12:15:29 +0100

    AdminServiceImplTest -> Refactor

--- BrainDissecting-SSR-version/src/test/java/com/antdevrealm/braindissectingssrversion/service/impl/AdminServiceImplTest.java
+++ BrainDissecting-SSR-version/src/test/java/com/antdevrealm/braindissectingssrversion/service/impl/AdminServiceImplTest.java
@@ -25,6 +25,10 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class AdminServiceImplTest {
 
+    private final long USER_ID = 1L;
+
+    private final String USERNAME = "testUser";
+
     @Mock
     private  UserService mockUserService;
     @Mock
@@ -37,26 +41,27 @@ public class AdminServiceImplTest {
 
     private AdminServiceImpl toTest;
 
+    private UserEntity userEntity;
+
     @BeforeEach
     void setUp() {
         toTest = new AdminServiceImpl(mockUserService, mockUserRepository, mockRoleRepository);
+
+        userEntity = new UserEntity();
+        userEntity.setId(USER_ID);
+        userEntity.setUsername(USERNAME);
+        userEntity.setRoles(new ArrayList<>());
     }
 
     @Test
     void promoteToModerator_ShouldReturnTrue_WhenUserExistsAndRoleIsAdded() {
-        long userId = 1L;
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(userId);
-        userEntity.setUsername("testUser");
-        userEntity.setRoles(new ArrayList<>());
-
         UserRoleEntity moderatorRole = new UserRoleEntity();
         moderatorRole.setRole(UserRole.MODERATOR);
 
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.of(userEntity));
         when(mockRoleRepository.findByRole(UserRole.MODERATOR)).thenReturn(Optional.of(moderatorRole));
 
-        boolean result = toTest.promoteToModerator(userId);
+        boolean result = toTest.promoteToModerator(USER_ID);
 
         Assertions.assertTrue(result);
         Assertions.assertTrue(userEntity.getRoles().contains(moderatorRole));
@@ -64,68 +69,49 @@ public class AdminServiceImplTest {
 
     @Test
     void promoteToModerator_ShouldReturnFalse_WhenUserDoesNotExist() {
-        long userId = 1L;
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.empty());
 
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.empty());
-
-        boolean result = toTest.promoteToModerator(userId);
+        boolean result = toTest.promoteToModerator(USER_ID);
 
         Assertions.assertFalse(result);
     }
 
     @Test
     void promoteToModerator_ShouldReturnFalse_WhenModeratorRoleDoesNotExist() {
-        long userId = 1L;
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(userId);
-        userEntity.setUsername("testUser");
-        userEntity.setRoles(new ArrayList<>());
-
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.of(userEntity));
         when(mockRoleRepository.findByRole(UserRole.MODERATOR)).thenReturn(Optional.empty());
 
-        boolean result = toTest.promoteToModerator(userId);
+        boolean result = toTest.promoteToModerator(USER_ID);
 
         Assertions.assertFalse(result);
     }
 
     @Test
     void promoteToModerator_ShouldReturnFalse_WhenUserAlreadyHasModeratorRole() {
-        Long userId = 1L;
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(userId);
-        userEntity.setUsername("testUser");
-
         UserRoleEntity moderatorRole = new UserRoleEntity();
         moderatorRole.setRole(UserRole.MODERATOR);
 
         userEntity.setRoles(new ArrayList<>(List.of(moderatorRole)));
 
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.of(userEntity));
         when(mockRoleRepository.findByRole(UserRole.MODERATOR)).thenReturn(Optional.of(moderatorRole));
 
-        boolean result = toTest.promoteToModerator(userId);
+        boolean result = toTest.promoteToModerator(USER_ID);
 
         Assertions.assertFalse(result);
     }
 
     @Test
     void demoteFromModerator_ShouldReturnTrue_WhenUserExistsAndRoleIsRemoved() {
-        long userId = 1L;
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(userId);
-        userEntity.setUsername("testUser");
-        userEntity.setRoles(new ArrayList<>());
-
         UserRoleEntity moderatorRole = new UserRoleEntity();
         moderatorRole.setRole(UserRole.MODERATOR);
         userEntity.getRoles().add(moderatorRole);
         Assertions.assertTrue(userEntity.getRoles().contains(moderatorRole));
 
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.of(userEntity));
         when(mockRoleRepository.findByRole(UserRole.MODERATOR)).thenReturn(Optional.of(moderatorRole));
 
-        boolean result = toTest.demoteFromModerator(userId);
+        boolean result = toTest.demoteFromModerator(USER_ID);
 
         Assertions.assertTrue(result);
         Assertions.assertFalse(userEntity.getRoles().contains(moderatorRole));
@@ -133,62 +119,43 @@ public class AdminServiceImplTest {
 
     @Test
     void demoteFromModerator_ShouldReturnFalse_WhenUserDoesNotExist() {
-        long userId = 1L;
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.empty());
 
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.empty());
-
-        boolean result = toTest.demoteFromModerator(userId);
+        boolean result = toTest.demoteFromModerator(USER_ID);
 
         Assertions.assertFalse(result);
     }
 
     @Test
     void demoteFromModerator_ShouldReturnFalse_WhenModeratorRoleDoesNotExist() {
-        long userId = 1L;
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(userId);
-        userEntity.setUsername("testUser");
-        userEntity.setRoles(new ArrayList<>());
-
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.of(userEntity));
         when(mockRoleRepository.findByRole(UserRole.MODERATOR)).thenReturn(Optional.empty());
 
-        boolean result = toTest.demoteFromModerator(userId);
+        boolean result = toTest.demoteFromModerator(USER_ID);
 
         Assertions.assertFalse(result);
     }
 
     @Test
     void demoteFromModerator_ShouldReturnFalse_WhenUserDoesNotHaveModeratorRole() {
-        Long userId = 1L;
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(userId);
-        userEntity.setUsername("testUser");
-
         UserRoleEntity moderatorRole = new UserRoleEntity();
         moderatorRole.setRole(UserRole.MODERATOR);
 
-        userEntity.setRoles(new ArrayList<>());
-
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.of(userEntity));
         when(mockRoleRepository.findByRole(UserRole.MODERATOR)).thenReturn(Optional.of(moderatorRole));
 
-        boolean result = toTest.demoteFromModerator(userId);
+        boolean result = toTest.demoteFromModerator(USER_ID);
 
         Assertions.assertFalse(result);
     }
 
     @Test
     void banUser_ShouldReturnTrue_WhenUserExistsAndIsActive() {
-        long userId = 1L;
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(userId);
-        userEntity.setUsername("'testUser'");
         userEntity.setStatus(UserStatus.ACTIVE);
 
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.of(userEntity));
 
-        boolean result = toTest.banUser(userId);
+        boolean result = toTest.banUser(USER_ID);
 
         Mockito.verify(mockUserRepository).save(userEntityCaptor.capture());
 
@@ -201,41 +168,31 @@ public class AdminServiceImplTest {
 
     @Test
     void banUser_ShouldReturnFalse_WhenUserIsAlreadyBanned() {
-        long userId = 1L;
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(userId);
-        userEntity.setUsername("'testUser'");
         userEntity.setStatus(UserStatus.BANNED);
 
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.of(userEntity));
 
-        boolean result = toTest.banUser(userId);
+        boolean result = toTest.banUser(USER_ID);
 
         Assertions.assertFalse(result);
     }
 
     @Test
     void banUser_ShouldReturnFalse_WhenUserDoesNotExist() {
-        long userId = 1L;
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.empty());
 
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.empty());
-
-        boolean result = toTest.banUser(userId);
+        boolean result = toTest.banUser(USER_ID);
 
         Assertions.assertFalse(result);
     }
 
     @Test
     void removeBan_ShouldReturnTrue_WhenUserExistsAndIsBanned() {
-        long userId = 1L;
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(userId);
-        userEntity.setUsername("'testUser'");
         userEntity.setStatus(UserStatus.BANNED);
 
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.of(userEntity));
 
-        boolean result = toTest.removeBan(userId);
+        boolean result = toTest.removeBan(USER_ID);
 
         Mockito.verify(mockUserRepository).save(userEntityCaptor.capture());
 
@@ -248,26 +205,20 @@ public class AdminServiceImplTest {
 
     @Test
     void removeBan_ShouldReturnFalse_WhenUserIsActive() {
-        long userId = 1L;
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(userId);
-        userEntity.setUsername("'testUser'");
         userEntity.setStatus(UserStatus.ACTIVE);
 
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.of(userEntity));
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.of(userEntity));
 
-        boolean result = toTest.removeBan(userId);
+        boolean result = toTest.removeBan(USER_ID);
 
         Assertions.assertFalse(result);
     }
 
     @Test
     void removeBan_ShouldReturnFalse_WhenUserDoesNotExist() {
-        long userId = 1L;
+        when(mockUserRepository.findById(USER_ID)).thenReturn(Optional.empty());
 
-        when(mockUserRepository.findById(userId)).thenReturn(Optional.empty());
-
-        boolean result = toTest.removeBan(userId);
+        boolean result = toTest.removeBan(USER_ID);
 
         Assertions.assertFalse(result);
     }
