7b770371df3d6879ba186fd0f081df0d1014fae8
AntoanYosifov <tapaktapxaomi@gmail.com>
2024-12-04 05:16:39 +0100

    Refactor templates head replace fragment

--- BrainDissecting-SSR-version/src/main/java/com/antdevrealm/braindissectingssrversion/web/AccessDeniedController.java
+++ BrainDissecting-SSR-version/src/main/java/com/antdevrealm/braindissectingssrversion/web/AccessDeniedController.java
@@ -3,8 +3,6 @@ package com.antdevrealm.braindissectingssrversion.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-// TODO: Add logic to prevent endpoint being reached by user with access
 @Controller
 public class AccessDeniedController {
 
